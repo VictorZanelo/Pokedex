4e7b334232b907daed9c1417581557e12e68349f
Victor  Zanelo <victorzanelo4@gmail.com>
2022-05-13 16:52:43 -0300

    api request

--- js/index.js
+++ js/index.js
@@ -1,5 +1,32 @@
-import axios from "axios";
+var nome = document.getElementById("nome");
+var sprite = document.getElementById("sprite");
+var input_name = document.getElementById("input_name");
+var caracteristicas = document.getElementById("caracteristicas");
 
+input_name.addEventListener("input", () => {
+  caracteristicas.innerHTML = "";
+  if (input_name.value != "") {
+    pegaPokemon(input_name.value);
+  } else {
+  }
+});
 
-
-axios.get()
+function pegaPokemon(value) {
+  axios
+    .get(`https://pokeapi.co/api/v2/pokemon/${value}`)
+    .then(function (response) {
+      console.log(response.data.forms[0].name);
+      let habilidades = response.data.abilities;
+      habilidades.forEach((element) => {
+        console.log(element.ability.name);
+        caracteristicas.innerHTML += `<div class="info"><div id="${response.data.forms[0].name}" class="btn-grad">${response.data.forms[0].name}</div> </div>
+    <div id="${element.ability.name}" class="info"><div class="btn-grad">${element.ability.name}</div></div>`;
+      });
+      sprite.innerHTML = ` <img class="crop" src="${response.data.sprites.other.home.front_default}"  width="100%" alt="">`;
+      nome.innerHTML = response.data.forms[0].name;
+      //res.send(`<img src="${image}" alt="">`)
+    })
+    .catch(function (error) {
+      console.error(error);
+    });
+}
