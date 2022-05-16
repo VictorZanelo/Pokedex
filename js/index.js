7dd6444981e6f588f8116157832ec244e5512e8e
Victor  Zanelo <victorzanelo4@gmail.com>
2022-05-16 15:56:04 -0300

    correções na responsividade

--- js/index.js
+++ js/index.js
@@ -6,7 +6,7 @@ var caracteristicas = document.getElementById("caracteristicas");
 input_name.addEventListener("input", () => {
   caracteristicas.innerHTML = "";
   if (input_name.value != "") {
-    pegaPokemon(input_name.value);
+    pegaPokemon(input_name.value.toLowerCase());
   } else {
   }
 });
@@ -17,12 +17,12 @@ function pegaPokemon(value) {
     .then(function (response) {
       console.log(response.data.forms[0].name);
       let habilidades = response.data.abilities;
+      caracteristicas.innerHTML = `<div class="info"><div id="${response.data.forms[0].name}" class="btn-grad">${response.data.forms[0].name}</div> </div>`
       habilidades.forEach((element) => {
         console.log(element.ability.name);
-        caracteristicas.innerHTML += `<div class="info"><div id="${response.data.forms[0].name}" class="btn-grad">${response.data.forms[0].name}</div> </div>
-    <div id="${element.ability.name}" class="info"><div class="btn-grad">${element.ability.name}</div></div>`;
+        caracteristicas.innerHTML += ` <div id="${element.ability.name}" class="info"><div class="btn-grad">${element.ability.name}</div></div>`;
       });
-      sprite.innerHTML = ` <img class="crop" src="${response.data.sprites.other.home.front_default}"  width="100%" alt="">`;
+      sprite.innerHTML = ` <img class="" src="${response.data.sprites.other.home.front_default}"  width="100%" alt="">`;
       nome.innerHTML = response.data.forms[0].name;
       //res.send(`<img src="${image}" alt="">`)
     })
