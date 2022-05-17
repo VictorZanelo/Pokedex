764c3e94fc7c8f195aeb158d9e5f3186602e11a9
Victor  Zanelo <victorzanelo4@gmail.com>
2022-05-17 10:01:45 -0300

    ajustes na responsividade e load adicionado

--- js/index.js
+++ js/index.js
@@ -2,6 +2,12 @@ var nome = document.getElementById("nome");
 var sprite = document.getElementById("sprite");
 var input_name = document.getElementById("input_name");
 var caracteristicas = document.getElementById("caracteristicas");
+var link = document.querySelector("link[rel~='icon']");
+link.href =
+  "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/items/sport-ball.png";
+var not_found = document.getElementById("not_found");
+var poke = document.getElementById("poke");
+var load = document.getElementById("load");
 
 input_name.addEventListener("input", () => {
   caracteristicas.innerHTML = "";
@@ -12,21 +18,29 @@ input_name.addEventListener("input", () => {
 });
 
 function pegaPokemon(value) {
+  poke.style.display = "none";
+  load.style.display = "block";
   axios
     .get(`https://pokeapi.co/api/v2/pokemon/${value}`)
     .then(function (response) {
-      console.log(response.data.forms[0].name);
-      let habilidades = response.data.abilities;
-      caracteristicas.innerHTML = `<div class="info"><div id="${response.data.forms[0].name}" class="btn-grad">${response.data.forms[0].name}</div> </div>`
+      //link.href = response.data.sprites.front_default ;
+      link.href = response.data.sprites.other.home.front_default;
+
+      let habilidades = response.data.types;
+      caracteristicas.innerHTML = `<div class="info"><div id="${response.data.forms[0].name}" class="btn-grad">${response.data.forms[0].name}</div> </div>`;
       habilidades.forEach((element) => {
-        console.log(element.ability.name);
-        caracteristicas.innerHTML += ` <div id="${element.ability.name}" class="info"><div class="btn-grad">${element.ability.name}</div></div>`;
+        caracteristicas.innerHTML += ` <div id="${element.type.name}" class="info"><div class="btn-grad">${element.type.name}</div></div>`;
       });
       sprite.innerHTML = ` <img class="" src="${response.data.sprites.other.home.front_default}"  width="100%" alt="">`;
       nome.innerHTML = response.data.forms[0].name;
-      //res.send(`<img src="${image}" alt="">`)
+      poke.style.display = "block";
+      load.style.display = "none";
     })
     .catch(function (error) {
-      console.error(error);
+      if (error.response.status == 404) {
+        load.style.display = "none";
+        poke.style.display = "none";
+        not_found.style.display = "block";
+      }
     });
 }
