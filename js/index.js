6be4088759eba8035a2082c1c152e7fa4e886dd8
Victor  Zanelo <victorzanelo4@gmail.com>
2022-05-17 15:03:15 -0300

    correção de bug no load e no not found

--- js/index.js
+++ js/index.js
@@ -1,12 +1,13 @@
 var nome = document.getElementById("nome");
-var sprite = document.getElementById("sprite");
+var img_poke = document.getElementById("img_poke");
 var input_name = document.getElementById("input_name");
+
 var caracteristicas = document.getElementById("caracteristicas");
 var link = document.querySelector("link[rel~='icon']");
 link.href =
   "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/items/sport-ball.png";
 var not_found = document.getElementById("not_found");
-var poke = document.getElementById("poke");
+//var poke = document.getElementById("poke");
 var load = document.getElementById("load");
 
 input_name.addEventListener("input", () => {
@@ -18,7 +19,8 @@ input_name.addEventListener("input", () => {
 });
 
 function pegaPokemon(value) {
-  poke.style.display = "none";
+  not_found.style.display = "none";
+  img_poke.style.display = "none";
   load.style.display = "block";
   axios
     .get(`https://pokeapi.co/api/v2/pokemon/${value}`)
@@ -31,16 +33,18 @@ function pegaPokemon(value) {
       habilidades.forEach((element) => {
         caracteristicas.innerHTML += ` <div id="${element.type.name}" class="info"><div class="btn-grad">${element.type.name}</div></div>`;
       });
-      sprite.innerHTML = ` <img class="sprite_poke" src="${response.data.sprites.other.home.front_default}"  width="100%" alt="">`;
-      nome.innerHTML = response.data.forms[0].name;
-      poke.style.display = "block";
+      
+      img_poke.innerHTML = ` <img class="sprite_poke" src="${response.data.sprites.other.home.front_default}"  width="100%" alt="">`;
+      //nome.innerHTML = response.data.forms[0].name;
+      img_poke.style.display = "block";
+      not_found.style.display = "none";
       load.style.display = "none";
     })
     .catch(function (error) {
-      if (error.response.status == 404) {
-        load.style.display = "none";
-        poke.style.display = "none";
-        not_found.style.display = "block";
-      }
+      console.log(error)
+      caracteristicas.innerHTML += ` <div class="info"><div class="btn-grad">404</div></div>`;
+      load.style.display = "none";
+      img_poke.style.display = "none";
+      not_found.style.display = "block";
     });
 }
